e457a4c5667e6d3776e9a3af63279dba2d482689
Michaela Arteberry <michaela.arteberry@gmail.com>
2025-01-13 10:56:43 -0500

    created a pages folder with home file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>
-        <h2 className="App">
-          Home Page
-        </h2>
+      <div className="App">
+          <Home />
       </div>
   )
 }
